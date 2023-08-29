2d4517af614c985d6e62f7236fe8f1b1ce5b8e9d
eliyya <tecno_eli@hotmail.com>
2023-08-28 20:15:49 -0600

    BooleanString kata

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
-import PhoneNumber;
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Kata.createPhoneNumber(new int[] {1,2,3,4,5,6,7,8,9,0}));
+        System.out.println(PhoneNumber.createPhoneNumber(new int[] {1,2,3,4,5,6,7,8,9,0}));
+        System.out.println(StringBoolean.boolToWord(true));
     }
 }
---
+++ StringBoolean.java
@@ -0,0 +1,27 @@
+/*
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+*/
+public class StringBoolean {
+    public static String boolToWord(boolean original) {
+        String innocence = "";
+        String naked = original + innocence;
+        int good = naked.length();
+        int holy = good + 1;
+        int trinity = 3;
+        boolean confusion = !!original;
+        String sinner = "Sinner";
+        int[] beast = {6,6,6};
+        String expulsion = innocence.replace("good", "evil");
+        for (int day = good; --day <= 7; day += trinity) {
+        int oppenheimer = day % trinity;
+        int death = beast[oppenheimer] - oppenheimer;
+        int evil = 7 * 7 - holy * holy + death;
+        char letter = sinner.charAt(evil % trinity);
+        confusion = ! confusion;
+        if (confusion) death = -death;
+        letter += death;
+        expulsion += letter;
+        }
+        return expulsion;
+    }
+}
